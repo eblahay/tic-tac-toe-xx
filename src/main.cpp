41dac56f0c724e737d97ea1e082f3ae018eb43e2
epbXIII <eprattblahay@gmail.com>
2020-11-25 04:01:33 -0500

    add victory determination funct & ui turn count & axis labels

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
+#include <cstring>
 
 #include "GameBoard.hpp"
 
-int main(){
+int main(int argc, char* argv[]){
+
     Gameboard gameboard(3, 3);
 
+    for(int i=0; i<argc; i++){
+        if(std::strcmp(argv[i], "--simple_grid")==0) gameboard.toggleSimpleGrid();
+    }
+
     gameboard.draw();
-    while(gameboard.getVictor()==Gameboard::GAME_ONGOING){
+    do{
 
         gameboard.handleInput();
 
         gameboard.draw(1);
     }
+    while(gameboard.determineVictor()==Gameboard::GAME_ONGOING);
+
     if(gameboard.getVictor()!=Gameboard::STALEMATE){
         std::cout << "Player " << gameboard.getVictor() << " wins!\n";
     }
     else{
         std::cout << "\nSTALEMATE!\n";
     }
+    std::cout << "Game Duration: " << gameboard.getTurnNumber() << " turns.\n";
 
     return 0;
 }
